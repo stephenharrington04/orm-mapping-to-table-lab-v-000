75b91b753cec806d9ab10746e8624ea5cd7a01f4
Stephen Harrington <stephenharrington04@yahoo.com>
2018-04-16 20:20:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,6 +17,7 @@ class Student
         grade INTEGER
         );
         SQL
+        
     DB[:conn].execute(sql)
   end
 
@@ -24,6 +25,7 @@ class Student
     sql = <<-SQL
       DROP TABLE students;
         SQL
+
     DB[:conn].execute(sql)
   end
 
@@ -32,7 +34,9 @@ class Student
       INSERT INTO students (name, grade)
       VALUES (? ,?);
     SQL
+
     DB[:conn].execute(sql, self.name, self.grade)
+
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
